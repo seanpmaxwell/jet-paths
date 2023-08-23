b266089e1adb76f5e38ac36a3b137b100cd49942
sean-concourse <sean@concourse.global>
2023-08-22 21:16:22 -0700

    added code

---
+++ README.md
@@ -0,0 +1,26 @@
+# About inserturlparams
+
+
+
+- This was written in Typescript and recommended for use with that.
+
+
+#### How it works
+
+
+
+#### Installation
+
+- `npm i -s jet-paths`
+
+
+#### Sample code:
+
+```typescript
+  import insertUrlParams, { TPrimitive, TObject } from 'inserturlparams';
+
+```
+
+
+
+Happy web-deving :)
---
+++ src/jetPaths.ts
@@ -0,0 +1,53 @@
+
+/**
+ * Convert paths to full paths.
+ */
+
+
+// **** Variables **** //
+
+const DEFAULT_BASE_KEY = 'Base';
+
+
+// **** Types **** //
+
+interface IPathObj {
+  [key: string]: string | IPathObj;
+}
+
+
+// **** Functions **** //
+
+function jetPaths(pathObj: IPathObj, baseKey?: string): IPathObj {
+  return jetPathsHelper(pathObj, baseKey ?? DEFAULT_BASE_KEY, '');
+}
+
+/**
+ * The recursive function.
+ */
+function jetPathsHelper(
+  parentObj: IPathObj,
+  baseKey: string,
+  baseUrl: string,
+): IPathObj {
+  // Init vars
+  const url = (baseUrl + parentObj.Base),
+    keys = Object.keys(parentObj),
+    retVal: IPathObj = { [baseKey]: url };
+  // Iterate keys
+  for (const key of keys) {
+    const pval = parentObj[key];
+    if (key !== baseKey && typeof pval === 'string') {
+      retVal[key] = (url + pval);
+    } else if (typeof pval === 'object') {
+      retVal[key] = jetPathsHelper(pval, baseKey, url);
+    }
+  }
+  // Return
+  return retVal;
+}
+
+
+// **** Export default **** //
+
+export default jetPaths;
